0f87a8efdf4e566a65fc88683618dae16fbe0056
32054 <3205482564@qq.com>
2026-02-05 15:27:43 +0800

    添加 lockdemo 后端项目源码

---
+++ lockdemo/src/main/java/cn/iocoder/cloud/lockdemo/LockdemoApplication.java
@@ -0,0 +1,13 @@
+package cn.iocoder.cloud.lockdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockdemoApplication.class, args);
+    }
+
+}
