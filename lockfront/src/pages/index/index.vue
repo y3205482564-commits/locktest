7f7380f8938660c72f2dda53cb42711bae0af5f5
32054 <3205482564@qq.com>
2026-02-09 10:48:51 +0800

    重新开始

--- lockfront/src/pages/index/index.vue
+++ lockfront/src/pages/index/index.vue
@@ -4,12 +4,6 @@
     <view class="text-area">
       <text class="title">{{ title }}</text>
     </view>
-    <view class="nav-list">
-      <view class="nav-item" @click="navigateToBlueLock">
-        <text class="nav-text">智能锁开锁页面</text>
-        <text class="nav-arrow">→</text>
-      </view>
-    </view>
   </view>
 </template>
 
@@ -17,17 +11,11 @@
 export default {
   data() {
     return {
-      title: 'UniApp 项目首页',
+      title: 'Hello',
     }
   },
   onLoad() {},
-  methods: {
-    navigateToBlueLock() {
-      uni.navigateTo({
-        url: '/pages/blueLock/blueLock'
-      })
-    }
-  },
+  methods: {},
 }
 </script>
 
@@ -37,7 +25,6 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 40rpx;
 }
 
 .logo {
@@ -52,43 +39,10 @@ export default {
 .text-area {
   display: flex;
   justify-content: center;
-  margin-bottom: 80rpx;
 }
 
 .title {
   font-size: 36rpx;
   color: #8f8f94;
-}
-
-.nav-list {
-  width: 100%;
-  max-width: 600rpx;
-}
-
-.nav-item {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 30rpx 40rpx;
-  margin-bottom: 20rpx;
-  background: #fff;
-  border-radius: 10rpx;
-  box-shadow: 0 2rpx 10rpx rgba(0, 0, 0, 0.1);
-  transition: all 0.3s;
-}
-
-.nav-item:active {
-  background: #f5f5f5;
-  transform: scale(0.98);
-}
-
-.nav-text {
-  font-size: 32rpx;
-  color: #333;
-}
-
-.nav-arrow {
-  font-size: 32rpx;
-  color: #999;
 }
 </style>
